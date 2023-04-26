052a2b17640b544922fd8c5b8b51fd39a5038eac
danielsm <dansmartins@hotmail.com>
2023-04-26 09:33:41 -0300

    transferindo classe Random para este repositorio

---
+++ randomNumbers.py
@@ -0,0 +1,24 @@
+class Random:
+    def __init__(self):
+        from random import randint   
+        self.num = randint(0,10)
+
+    def JogoAdivinhacao(self):
+        a = int(0)
+        while a == 0:
+            numChoice = int(input('Digite um número de 0 a 10:\n '))
+            if numChoice == self.num:
+                print(f' Parabéns, vc acertou o número, resposta: {self.num}')
+                a += 1
+            elif numChoice > 10:
+                print('número invalido, escolha um número entre 0 e 10')
+            elif numChoice != self.num:
+                print(' número errado, tente novamente')
+            else:
+                print('erro')
+
+
+adivinhacao= Random()
+
+adivinhacao.JogoAdivinhacao()
+
