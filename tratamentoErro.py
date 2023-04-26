64c4753333eb46e3ace157a52a8a44f11f673ee7
danielsm <dansmartins@hotmail.com>
2023-04-26 06:40:56 -0300

    correção de comentario no teste de tratamento de erro

--- tratamentoErro.py
+++ tratamentoErro.py
@@ -4,7 +4,8 @@ a = int(0)
 while a == 0:    
     try:
             numero = int(input('digite um número ')) 
-            break #o codigo ira tentar executar a linha de cima, caso seja inserido pelo usuario um numero tipo int, o break é executado e para o laço, se não, executa o except e o laço continua até o usuario digitar um número
+            break #o codigo ira tentar executar a linha de cima, caso seja inserido pelo usuario um numero tipo int, o break é executado e para o laço, 
+            #se não, executa o except e o laço continua até o usuario digitar um número 
            
             
 
